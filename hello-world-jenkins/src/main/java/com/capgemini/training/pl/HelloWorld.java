d2b7565c42adfdb4484973aa70cbb69e31ebf9f1
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2020-12-25 00:45:54 +0530

    first

---
+++ hello-world-jenkins/src/main/java/com/capgemini/training/pl/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.capgemini.training.pl;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("ThanQ");
+
+	}
+
+}
